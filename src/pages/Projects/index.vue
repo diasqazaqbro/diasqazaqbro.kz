02c85bd8e3061f1bf2dc87380d3b0e0bc36d974a
diasqazaqbro <diasqazaqbro@mail.ru>
2023-12-05 17:52:21 +0600

    feat: add new project

--- src/pages/Projects/index.vue
+++ src/pages/Projects/index.vue
@@ -3,8 +3,12 @@
     <Breadcrumb :breadcrumb='breadcrumb' />
     <div class='project'>
       <h5>Активные</h5>
-      <ProjectItem :info='active' />
+      <ProjectItem :info='one' />
+      <ProjectItem :info='four' />
+      <ProjectItem :info='two' />
+      <ProjectItem :info='three' />
       <h5>Архив</h5>
+      <ProjectItem :info='archive' />
     </div>
   </div>
 </template>
@@ -24,7 +28,7 @@ export default {
         isArticle: false
       },
       showDetails: false,
-      active: [
+      one: [
         {
           title: 'diasqazaqbro v2',
           description: 'Сайт-визитка',
@@ -38,7 +42,58 @@ export default {
               link: ''
             }
           ]
-        }
+        },
+      ],
+      two: [
+        {
+          title: 'TopElectro',
+          description: 'React-приложения для компании',
+          icons: 'se',
+          details: 'В этом проекте я был ответственным за разработку и поддержку React-приложения, которое предоставляло оздание SPA с использование Bootstrap’ом. Я работал в тесном сотрудничестве с дизайнерами, чтобы создать удобный и нтуитивно понятный пользовательский интерфейс.',
+          links: [
+            {
+              name: 'Сам сайт',
+              color: 'blue',
+              icon: 'fa fa-globe',
+              link: 'https://topecar.kz/'
+            }
+          ]
+        },
+      ],
+      three: [
+        {
+          title: 'TrustTravel',
+          description: 'Start-up',
+          icons: 'se',
+          details: 'Разработка стартапа где я CTO. Сайт написан на Next.js - это обертка над React которая дает SSR рендеринг.',
+          links: [
+
+          ]
+        },
+      ],
+      four: [
+        {
+          title: 'Hudos.kz',
+          description: 'Коммерчиский сайт для Архитекторов.',
+          icons: 'se',
+          details: 'Что сделал я: Создал и подключил админскую панель. Редизайн front-end части существующих веб-сайтов в едином минималистичном стиле, связывание с предоставленным заказчиком Web API.',
+          links: [
+          {
+              name: 'Сам сайт',
+              color: 'blue',
+              icon: 'fa fa-globe',
+              link: 'https://hudos.vercel.app/'
+            }
+          ]
+        },
+      ],
+      archive: [
+        {
+          title: 'Разработка PHP-сайта для бизнеса',
+          description: 'Сайт-для компаний',
+          icons: 'dsad',
+          details: 'В этом проекте я был ответственным за разработку PHP-сайта для бизнеса. Задача сайта состояла в предоставлении информации о компании, ее продуктах и услугах, а также обеспечении возможности клиентам связаться с компанией.',
+        },
       ]
     }
   },
